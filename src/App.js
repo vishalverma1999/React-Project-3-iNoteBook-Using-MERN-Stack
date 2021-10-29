9c960f6fd7db295459748acec50db1d9f44bb2dd
vishalverma1999 <vermavishal1999.vv@gmail.com>
2021-10-29 13:32:08 +0530

    #58 useContext hook: Using React Context API

--- src/App.js
+++ src/App.js
@@ -7,10 +7,12 @@ import {
 import Navbar from './components/Navbar';
 import { Home } from './components/Home';
 import About from './components/About';
+import NoteState from './context/notes/NoteState';
 
 function App() {
   return (
     <>
+    <NoteState>
       <Router>
         <Navbar />
         <Switch>
@@ -22,6 +24,7 @@ function App() {
           </Route>
         </Switch>
       </Router>
+      </NoteState>
     </>
   );
 }
